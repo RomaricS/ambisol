70c395f366a16ae09fe75ce7c53876de247f6f92
Romaric SEGLA <romaricboss@gmail.com>
2025-02-13 14:48:56 +0100

    hide api keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk2lPBey3alZPajAhH2g-z0hMxXoLeR1A",
-  authDomain: "ambisol.firebaseapp.com",
-  projectId: "ambisol",
-  storageBucket: "ambisol.firebasestorage.app",
-  messagingSenderId: "622685809740",
-  appId: "1:622685809740:web:b35b77ffbf767c1b3158e9",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
