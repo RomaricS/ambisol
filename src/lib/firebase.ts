d0fbb7b377984bd1c4035e42f2c378229b95ad92
Romaric SEGLA <romaricboss@gmail.com>
2025-02-13 17:08:25 +0100

    test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,8 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+console.log(process)
+console.log(import.meta)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
